bfb16cca8305df58fd51c3df262e5ae057aa0e50
Your Name <ayush.saxena@sparxitsolutions.com>
2022-12-30 20:42:11 +0530

    backup

--- javascript/assests/js/custom.js
+++ javascript/assests/js/custom.js
@@ -7,3 +7,32 @@ const presentYear = 2022 ;
 const ayush = presentYear - 1997;
 const ayushi = presentYear - 1998;
 console.log(ayush * 2, ayushi / 2);
+
+// test first 
+// calculating and comparing BMI of two person
+
+//BMI = mass / height **2 = mass /  (height * height). (mass will be in kg and height in meter).
+
+const storeMass = 95 ;
+const storeHeight = 1.88 ;
+const johnMass = 85 ;
+const johnHeight = 1.76 ;
+
+const storeBmi = storeMass / storeHeight ** 2;
+console.log(storeMass);
+
+const johnBmi = storeMass / (johnHeight * johnHeight);
+console.log(johnBmi);
+
+const storeHeigherBMI =  storeBmi > johnBmi;
+console.log(storeHeigherBMI);
+
+//strings and Template Literals
+
+const firstName = "jonas";
+const job = "developer";
+const birthyear = "1997";
+const year = 2022;
+
+const description = "I' am " + firstName + " a " + job + " with a age of " + (year - birthyear);
+console.log(description);
