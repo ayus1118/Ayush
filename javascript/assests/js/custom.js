4cc5a108cf887757e781613c6a75626146133b9c
Ayush Saxena <ayush.saxena@sparxitsolutions.com>
2023-01-04 23:06:42 +0530

    code push

--- javascript/assests/js/custom.js
+++ javascript/assests/js/custom.js
@@ -79,3 +79,29 @@ console.log(String(23) + birthYearTest + ` is the number`);
 console.log(`my number is ` + 23);
 console.log(23 + 42);
 console.log(`423` - `42` - 3);
+
+//falsy values
+// there are 5 falsy values :- 0, '' , undefined , null , NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 0;  //zero is falsy value soo we get the else answer
+if (money){
+    console.log('dont have'); 
+}
+else{
+    console.log('you should have a job');
+}
+
+
+let height;
+if (height){
+    console.log('height is defined');
+}
+else{
+    console.log('height is undefined');
+}
