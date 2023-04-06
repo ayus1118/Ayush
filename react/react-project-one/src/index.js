1b36c20e10bfba5e2a4df9fdb263a64e22c6527c
Your Name <ayush.saxena@sparxitsolutions.com>
2023-04-06 12:15:19 +0530

    office push

--- react/react-project-one/src/index.js
+++ react/react-project-one/src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"; // IF WE WANT TO USE HTML MARK UP IN REACT THEN WE HAVE TO IMPORT REACT MODULE
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <h1>hello world</h1>
+  <h1>hello world</h1> // JSX EXPRESSION
 );
 
-ReactDOM.render(<h1> hello world </h1> , document.getElementById("root"));
+ReactDOM.render(<h1> hello world </h1>, document.getElementById("root-new"));
+// in react v16 it is possible for render() to return an array of elements.
+ReactDOM.render(
+  [<h1> hello world </h1>, <p> lets start</p>],
+  document.getElementById("root-three")
+);
 
 reportWebVitals();
 export default App;
