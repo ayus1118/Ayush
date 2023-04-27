521a4c2d7aba45943f672117f9e7c9c66415b300
Your Name <ayush.saxena@sparxitsolutions.com>
2023-04-27 19:40:54 +0530

    react-component

---
+++ react/react-component/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <h1>react components</h1>
+    <ol>
+      <li>data 1</li>
+      <li>data 2</li>
+      <li>data 3</li>
+      <li>data 4</li>
+    </ol>
+  </>
+);
+
+reportWebVitals();
