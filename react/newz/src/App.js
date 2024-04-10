0cbd1c19eb81968d42c03febf20f137cbab26a7d
ayush <ayushgyu@gmail.com>
2024-04-10 17:10:11 +0530

    newz bootsrap update

--- react/newz/src/App.js
+++ react/newz/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.jsx';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import 'bootstrap/dist/js/bootstrap.min.js';
+import "bootstrap/dist/js/bootstrap.bundle.js"
 
 function App() {
   return (
-    <header/>
+       <>
+       <Header/>
+       </>
   );
 }
 
