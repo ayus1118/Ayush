247ecc884549af167d03dea42dfa59f708e829c7
ayush <ayushgyu@gmail.com>
2025-04-09 17:22:51 +0530

    new setup

---
+++ viva/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <>
+      <div className="container">
+          <p>ayush</p>
+        </div>
+    </>
+  );
+}
+export default App;
