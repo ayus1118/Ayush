31d5b99526e3733d9d76dd4799d268d1e2cd5766
ayush <ayushgyu@gmail.com>
2025-05-07 18:15:17 +0530

    css file

--- react/harry_task/src/components/Main.js
+++ react/harry_task/src/components/Main.js
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'jquery/dist/jquery.min.js'
 import '../App.css';
-import Header from './Header/Header';
+import Header from './Header/Header.jsx';
 function Main() {
   return (
    <>
